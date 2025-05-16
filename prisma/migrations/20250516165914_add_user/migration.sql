da74893c916c7880e8d638bf4853e48dbbf1c590
Bona_Kim <bona2324@gmail.com>
2025-05-17 02:02:37 +0900

    prisma setting

---
+++ prisma/migrations/20250516165914_add_user/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "username" TEXT NOT NULL,
+    "email" TEXT,
+    "password" TEXT,
+    "phone" INTEGER,
+    "avatar" TEXT,
+    "createAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updateAt" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");
